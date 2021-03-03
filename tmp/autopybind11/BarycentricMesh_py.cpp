e7e4f7251ce0187501f82072d1888ef2a80eddb8
Joseph Snyder <joe.snyder@kitware.com>
2021-03-03 13:18:07 -0500

    Update AutoPyBind11:
    Adds update which will replace long template params with shorter
    alias names if used.

--- tmp/autopybind11/BarycentricMesh_py.cpp
+++ tmp/autopybind11/BarycentricMesh_py.cpp
@@ -99,12 +99,63 @@ void apb11_pydrake_BarycentricMesh_py_register(py::module &m) {
                            Eigen::InnerStride<1>> const &input) {
              return self.Eval(mesh_values, input);
            })
+      .def("EvalBarycentricWeights",
+           [](::drake::math::BarycentricMesh<double> &self,
+              ::Eigen::Ref<const Eigen::Matrix<double, -1, 1, 0, -1, 1>, 0,
+                           Eigen::InnerStride<1>> const &input,
+              Eigen::Ref<::drake::EigenPtr<Eigen::Matrix<int, -1, 1, 0, -1, 1>>,
+                         0, Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>>
+                  mesh_indices,
+              Eigen::Ref<
+                  ::drake::EigenPtr<Eigen::Matrix<double, -1, 1, 0, -1, 1>>, 0,
+                  Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>>
+                  weights) {
+             return self.EvalBarycentricWeights(input, mesh_indices, weights);
+           })
       .def("MeshValuesFrom",
            [](::drake::math::BarycentricMesh<double> &self,
               ::std::function<Eigen::Matrix<double, -1, 1, 0, -1, 1>(
                   const Eigen::Ref<const Eigen::Matrix<double, -1, 1, 0, -1, 1>,
                                    0, Eigen::InnerStride<1>> &)> const
                   &vector_func) { return self.MeshValuesFrom(vector_func); })
+      .def(
+          "get_all_mesh_points",
+          static_cast<::Eigen::Matrix<double, -1, -1, 0, -1, -1> (
+              ::drake::math::BarycentricMesh<double>::*)() const>(
+              &::drake::math::BarycentricMesh<double>::get_all_mesh_points),
+          R"""(/// Returns a matrix with all of the mesh points, one per column.)""")
+      .def(
+          "get_input_grid",
+          static_cast<::std::vector<
+              std::set<double, std::less<double>, std::allocator<double>>,
+              std::allocator<std::set<double, std::less<double>,
+                                      std::allocator<double>>>> const
+                          &(::drake::math::BarycentricMesh<double>::*)() const>(
+              &::drake::math::BarycentricMesh<double>::get_input_grid))
+      .def("get_input_size",
+           static_cast<int (::drake::math::BarycentricMesh<double>::*)() const>(
+               &::drake::math::BarycentricMesh<double>::get_input_size))
+      .def("get_mesh_point",
+           [](::drake::math::BarycentricMesh<double> &self, int index,
+              Eigen::Ref<
+                  ::drake::EigenPtr<Eigen::Matrix<double, -1, 1, 0, -1, 1>>, 0,
+                  Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>>
+                  point) { return self.get_mesh_point(index, point); })
+      .def(
+          "get_mesh_point",
+          static_cast<::Eigen::Matrix<double, -1, 1, 0, -1, 1> (
+              ::drake::math::BarycentricMesh<double>::*)(int) const>(
+              &::drake::math::BarycentricMesh<double>::get_mesh_point),
+          py::arg("index"),
+          R"""(/// Returns the position of a mesh point in the input space referenced by its 
+/// scalar index to @p point. 
+/// @param index must be in [0, get_num_mesh_points).)""")
+      .def("get_num_interpolants",
+           static_cast<int (::drake::math::BarycentricMesh<double>::*)() const>(
+               &::drake::math::BarycentricMesh<double>::get_num_interpolants))
+      .def("get_num_mesh_points",
+           static_cast<int (::drake::math::BarycentricMesh<double>::*)() const>(
+               &::drake::math::BarycentricMesh<double>::get_num_mesh_points))
 
       ;
 }
--- tools/workspace/autopybind11_py/repository.bzl
+++ tools/workspace/autopybind11_py/repository.bzl
@@ -9,8 +9,8 @@ def autopybind11_py_repository(
         name = name,
         # Temporary branch
         repository = "autopybind11/autopybind11",
-        commit = "1f01929a18014fd8c7868f30ae50526b07295cd3",
-        sha256 = "50f28de610031e2a2024d3365d48c419a9c6554d72b6ccd123c8680ec289b7ff",  # noqa
+        commit = "89fecfb3a3c763f6ca63106796ff5c9e6de63872",
+        sha256 = "e4293b658d9b5af837ed86a0cd5915eda35c6a0833093ca5bfe9fad1364b839c",  # noqa
         build_file = "@drake//tools/workspace/autopybind11_py:package.BUILD.bazel",  # noqa
         mirrors = {
             "github": ["https://gitlab.kitware.com/{repository}/-/archive/{commit}/autopybind11-{commit}.tar.gz"],  # noqa
